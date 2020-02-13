b4f66c6948212f1dd325e33b1c8c341d26941c26
cutmuetia <57970261+cutmuetia@users.noreply.github.com>
2020-02-13 20:40:18 +0700

    Revert "Revert 2 auto update"

--- src/js/storage.js
+++ src/js/storage.js
@@ -926,7 +926,6 @@
     [ 'env_chromium', 'chromium' ],
     [ 'env_edge', 'edge' ],
     [ 'env_firefox', 'firefox' ],
-    [ 'env_legacy', 'legacy' ],
     [ 'env_mobile', 'mobile' ],
     [ 'env_safari', 'safari' ],
     [ 'cap_html_filtering', 'html_filtering' ],
